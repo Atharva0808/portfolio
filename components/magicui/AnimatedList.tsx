4afc9d8652474bed7a49bfcaa7e1c46002f62977
Atharva Bamgude <bamgudeatharva@gmail.com>
2026-01-09 20:08:11 +0530

    Fix framer-motion typing issue for Vercel build 2nd time

--- components/magicui/AnimatedList.tsx
+++ components/magicui/AnimatedList.tsx
@@ -35,16 +35,17 @@ export const AnimatedList = React.memo(
 );
 AnimatedList.displayName = "AnimatedList";
 export function AnimatedListItem({ children }: { children: React.ReactNode }) {
-    const animations = {
-        initial: { scale: 0, opacity: 0 },
-        animate: { scale: 1, opacity: 1, originY: 0 },
-        exit: { scale: 0, opacity: 0 },
-        transition: { stiffness: 350, damping: 40 },
-    };
-
     return (
-        <motion.div {...animations} layout className="mx-auto w-full">
+        <motion.div
+            layout
+            className="mx-auto w-full"
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1, originY: 0 }}
+            exit={{ scale: 0, opacity: 0 }}
+            transition={{ stiffness: 350, damping: 40 }}
+        >
             {children}
         </motion.div>
+
     );
 }
